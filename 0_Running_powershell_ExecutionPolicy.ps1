1a90171c00ba6cb24f9d531d86081513fd3949f5
L00186142 <l00186142@atu.ie>
2023-11-23 20:20:41 +0000

    Adding up the powershell scripts that were created from the notes

---
+++ 0_Running_powershell_ExecutionPolicy.ps1
@@ -0,0 +1,1 @@
+powershell.exe -executionpolicy bypass -File '.\Verify PowerShell 7.ps1'
---
+++ 3_Install_Powershell7.ps1
@@ -0,0 +1,9 @@
+﻿$MYPARMS = @{
+    UseMSI = $true
+    Quiet = $true
+    AddExplorerContextMenu = $true
+    EnablePSRemoting = $true
+
+
+}
+C:\PowerShell\Install-PowerShell.ps1 @MYPARMS

dce0f520563431e4467edc5adc29f548334632ec
L00186142 <l00186142@atu.ie>
2023-12-14 22:21:47 +0000

    Testing Merge and Conflict on Github

---
+++ RemoteControl/build.ps1
@@ -0,0 +1,17 @@
+Install-WindowsFeature -name AD-Domain-Services –IncludeManagementTools
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "ads.kmn.ie" `
+-DomainNetbiosName "ads" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
+Shutdown /r /t 0
